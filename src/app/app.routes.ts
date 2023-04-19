e3bbfca8d14915a20c237baf973c8b4c794b4d26
Rakesh Das <bcarakesh03@gmail.com>
2023-04-19 10:25:45 +0530

    added login , register pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,6 +14,14 @@ export const routes: Routes = [
   {
     path: 'loader',
     loadComponent: () => import('./pages/loader/loader.page').then( m => m.LoaderPage)
+  },  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
   },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+  },
+
 
 ];
